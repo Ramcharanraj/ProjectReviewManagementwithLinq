5f7f345b818adeadd34126dc36281a18ef858be5
Ramcharanraj <Ramcharanraj94@gmail.com>
2021-10-25 02:22:47 +0530

    ADDED: UC-1

--- ProjectReviewManagementwithLinq/ProductReview.cs
+++ ProjectReviewManagementwithLinq/ProductReview.cs
@@ -4,7 +4,31 @@ namespace ProjectReviewManagementwithLinq
     public class ProductReview
     {
         public ProductReview()
+      
+            public int ProductID
         {
+            get;
+            set;
+        }
+        public int UserID
+        {
+            get;
+            set;
+        }
+        public int Rating
+        {
+            get;
+            set;
+        }
+        public string Review
+        {
+            get;
+            set;
+        }
+        public bool isLike
+        {
+            get;
+            set;
         }
     }
 }
--- ProjectReviewManagementwithLinq/Program.cs
+++ ProjectReviewManagementwithLinq/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ProjectReviewManagementwithLinq
 {
@@ -6,7 +8,23 @@ namespace ProjectReviewManagementwithLinq
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Product review management problem statement");
+
+            List<ProductReview> lineProductReview = new List<ProductReview>()
+            {
+                new ProductReview() { ProductID = 1, UserID = 1, Rating = 5, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 2, UserID = 1, Rating = 5, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 3, UserID = 3, Rating = 5, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 4, UserID = 5, Rating = 5, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 5, UserID = 4, Rating = 5, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 6, UserID = 6, Rating = 5, Review = "Good", isLike = true }
+        };
+
+            foreach { var list in productReviewList}
+            {
+                Console.WriteLine("ProductID" + list.ProductID +" " + "UserId:-"+ list.UserID+ nameof+ " + "+ Rating:- +"  List.Rating  " " "Review:-" + list.Review +" ""+" "isLike:-"+ list.isLike);
+            }
+
         }
     }
 }
